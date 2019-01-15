47365f8f160d0a31601e0e4e6b78f0ab4daa9e57
Jamie Chen <jc7699@nyu.edu>
2019-01-15 03:11:35 -0500

    Add files via upload

--- DungeonProject/scenes/Character.gd
+++ DungeonProject/scenes/Character.gd
@@ -6,6 +6,7 @@ var screensize
 var velocity = Vector2()
 var action = ""
 var attackStyle = ""
+var is_on_floor = true
 
 func _ready():	
 	screensize = get_viewport_rect().size
@@ -31,16 +32,32 @@ func get_input():
 		action = "attacking"
 		attackStyle = "heavy"
 	if Input.is_action_just_released('lightattack') || Input.is_action_just_released('heavyattack'):
-		action = "none"
+		action = "none"	
+	if Input.is_action_pressed('jump') && action != "attacking" && is_on_floor == true:
+		action = "jumping"
+		is_on_floor = false
+	if Input.is_action_pressed('slide') && action != "attacking" && action != "sliding":
+		action = "sliding"
+			
 	velocity = velocity.normalized() * speed
+	
 	if action == "running" && velocity.length() > 0:
-		$AnimatedSprite.play("running")
 		$AnimatedSprite.flip_v = false
 		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimatedSprite.play("running")
 	elif action == "attacking" && attackStyle == "light":
 		$AnimatedSprite.play('lightattack')
 	elif action == "attacking" && attackStyle == "heavy":
 		$AnimatedSprite.play('heavyattack')
+	elif action == "jumping":
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimatedSprite.play('jump')
+		is_on_floor = true
+	elif action == "sliding":
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimatedSprite.play('slide')
 	else:
 		$AnimatedSprite.play("idle")
 
